10ddbd8ef4cbf738b287cfebbe15d289636be324
magnus <magnus.wahlstrand@gmail.com>
2023-07-23 10:17:19 +0200

    mobile view complete

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -5,7 +5,7 @@ import {Button} from "@/components/ui/button"
 import {CartItem} from "@/lib/types";
 import {ShoppingCart} from "lucide-react";
 import {Badge} from "@/components/ui/badge";
-import {CartRow, CartRow2} from "@/components/CartRow";
+import {CartRow2} from "@/components/CartRow";
 
 type Props = {
     items: CartItem[]
@@ -30,7 +30,7 @@ function OpenCartButtonV2(props: { totalQuantity: number, onToggleCartOpen: () =
                 Time Bubble
             </div>
             <button
-                className="text-background flex flex-row items-center hover:bg-muted-foreground rounded-md gap-2 px-3 py-1"
+                className="text-background flex flex-row items-center rounded-md gap-2 px-3 py-1"
                 onClick={() => props.onToggleCartOpen()}
             >
                 <div ref={ref} onAnimationEnd={removeSpinAnimation}>
@@ -39,10 +39,6 @@ function OpenCartButtonV2(props: { totalQuantity: number, onToggleCartOpen: () =
                 <ShoppingCart className="h-6 w-6"/>
             </button>
         </div>)
-}
-
-type CartContentProps = Props & {
-    closeCart: () => void
 }
 
 
@@ -65,45 +61,6 @@ function CartEmpty() {
     </div>;
 }
 
-function CartContent({items, onRemoveProduct, redirectToPayment, closeCart}: CartContentProps) {
-    async function action() {
-        await redirectToPayment(items)
-    }
-
-    const totalPrice = items.reduce((total, item) => total + item.quantity * item.price.amount, 0)
-    const totalQuantity = items.reduce((total, item) => total + item.quantity, 0)
-
-
-    if (totalQuantity < 1) {
-        return <CartEmpty/>
-    }
-
-
-    return (
-        <div className="w-full">
-            <ul className={""}>
-                {items.map((item) => (
-                    <CartRow
-                        key={item.id} item={item}
-                        onRemoveItem={() => onRemoveProduct(item.id)}
-                    />
-                ))}
-            </ul>
-            <div className={"flex flex-col"}>
-                <Subtotal totalPrice={totalPrice}/>
-                <div className="flex flex-col w-3/4 gap-2">
-                    <form className="w-full mt-2 flex justify-center" action={action}>
-                        <Button className="w-full" type="submit"
-                                disabled={totalQuantity < 1}>Checkout</Button>
-                    </form>
-                    <Button className="w-full" variant="outline"
-                            onClick={closeCart}>Close</Button>
-                </div>
-            </div>
-        </div>
-    )
-}
-
 export default function WrappedCart(props: Props) {
     const [height, setHeight] = useState(0);
 
@@ -113,8 +70,7 @@ export default function WrappedCart(props: Props) {
     useLayoutEffect(() => {
         if (ref.current && ref.current.clientHeight) {
             const height = ref.current.clientHeight;
-            console.log('call', height)
-            setHeight(height + 1);
+            setHeight(height);
         }
     }, [props.items])
 
@@ -130,40 +86,33 @@ export default function WrappedCart(props: Props) {
     return <div className="flex flex-row justify-center w-full">
         <div className="w-full">
             <OpenCartButtonV2 totalQuantity={totalQuantity} onToggleCartOpen={toggleCartOpen}/>
-            <div className={"py-0 m-5"}>
-                {totalQuantity === 0 && <CartEmpty/>}
-                {totalQuantity > 0 && <form action={action}>
-
-                    {/*header */}
-                    <h3 className="font-semibold text-xl">Your cart</h3>
-                    {/*list*/}
-                    <ul>
-                        {props.items.map((item) => (
-                            <CartRow2
-                                key={item.id} item={item}
-                                onRemoveItem={() => props.onRemoveProduct(item.id)}
-                            />
-                        ))}
-                    </ul>
-                    <Subtotal totalPrice={totalPrice}/>
-                    {/*go to payment*/}
-                    <Button className="mt-4 w-full" type="submit">Checkout</Button>
-                </form>}
-                {/*close checkout*/}
-                <Button className="mt-4 w-full" variant="outline" onClick={() => setCartOpen(false)}>Close</Button>
+            <div className={`overflow-hidden transition-max-height duration-300 shadow`} style={{
+                maxHeight: cartOpen ? `${height}px` : "0px",
+            }}>
+                <div className={"p-4"} ref={ref}>
+                    {totalQuantity === 0 && <CartEmpty/>}
+                    {totalQuantity > 0 && <form action={action}>
+                        {/*header */}
+                        <h3 className="font-semibold text-xl">Your cart</h3>
+                        {/*list*/}
+                        <ul>
+                            {props.items.map((item) => (
+                                <CartRow2
+                                    key={item.id} item={item}
+                                    onRemoveItem={() => props.onRemoveProduct(item.id)}
+                                />
+                            ))}
+                        </ul>
+                        <Subtotal totalPrice={totalPrice}/>
+                        {/*go to payment*/}
+                        <Button className="mt-4 w-full" type="submit">Checkout</Button>
+                    </form>}
+                    {/*close checkout*/}
+                    <Button className="mt-4 w-full" variant="outline" onClick={() => setCartOpen(false)}>Close</Button>
+                </div>
             </div>
-            <div className="bg-indigo-700 h-6"></div>
-            {/*<div className={`overflow-hidden transition-max-height duration-300 border-b-4 border-foreground`} style={{*/}
-            {/*    maxHeight: cartOpen ? `${height}px` : "0px",*/}
-            {/*}}>*/}
-            {/*    <div ref={ref}>*/}
-            {/*        <div className={"p-4 border-t-4 border-foreground flex flex-col items-center"}>*/}
-            {/*            <div className={"max-w-md w-full"}>*/}
-            {/*                <CartContent {...props} closeCart={() => setCartOpen(false)}/>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div className="bg-lime-300 h-1"></div>
+            <div className="bg-lime-100 h-1"></div>
         </div>
     </div>
 }
